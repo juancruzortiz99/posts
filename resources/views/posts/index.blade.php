12f2b73bd8edb39dbb7a91446f6539ec25de9875
juancruz99 <cabezotagigante99@gmail.com>
2023-03-06 08:25:38 +0100

    6to commit

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,12 +5,19 @@
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             @foreach ($posts as $post)
                 <article class="w-full h-80 bg-cover bg-center @if ($loop->first) md:col-span-2 @endif"
-                    style="background-image: url(@if($post->image) {{ Storage::url($post->image->url) }}@else https://cdn.pixabay.com/photo/2022/11/10/20/44/switzerland-7583676_960_720.jpg @endif)">
+                    style="background-image: url(@if ($post->image) {{ Storage::url($post->image->url) }}@else https://cdn.pixabay.com/photo/2022/11/10/20/44/switzerland-7583676_960_720.jpg @endif)">
                     <div class="w-full h-full px-8 flex flex-col justify-center">
                         <div>
                             @foreach ($post->tags as $tag)
-                                <a href="{{ route('posts.tag', $tag) }}"
-                                    class="inline-block px-3 h-6 text-white rounded-full" style="background-color: {{ $tag->color }};">{{ $tag->name }}</a>
+                                @if ($tag->color == 'yellow')
+                                    <a href="{{ route('posts.tag', $tag) }}"
+                                        class="inline-block px-3 h-6 text-white rounded-full"
+                                        style="background-color: {{ $tag->color }}; color:black;"><strong>{{ $tag->name }}</strong></a>
+                                @else
+                                    <a href="{{ route('posts.tag', $tag) }}"
+                                        class="inline-block px-3 h-6 text-white rounded-full"
+                                        style="background-color: {{ $tag->color }}; color:white;"><b>{{ $tag->name }}</b></a>
+                                @endif
                             @endforeach
                         </div>
                         <h1 class="text-4xl text-white leading-8 font-bolt">
